fa549f734652293ae0c9d21adcde8fd3984d2388
robbobbinett <robinettryan8@gmail.com>
2020-02-25 12:51:02 -0600

    computing eigenbasis for each cluster

--- experiments/unit_sphere.py
+++ experiments/unit_sphere.py
@@ -15,7 +15,7 @@ def sample_spherical(npoints, ndim=3):
 	vec /= np.linalg.norm(vec, axis=0)
 	return vec
 
-n = 100
+n = 2000
 d = 3
 
 sphere = sample_spherical(n, d)
@@ -48,28 +48,35 @@ for j in range(n):
 	for k in range(n):
 		dist_mat[j, k] = np.linalg.norm(sphere_jit[:, j]-sphere_jit[:, k])
 
-def k_fig(k):
+def k_fig(k, show=False):
 	M, C = kMedoids(dist_mat, k)
-	phi = np.linspace(0, np.pi, 20)
-	theta = np.linspace(0, 2 * np.pi, 40)
-	x = np.outer(np.sin(theta), np.cos(phi))
-	y = np.outer(np.sin(theta), np.sin(phi))
-	z = np.outer(np.cos(theta), np.ones_like(phi))
+	if show:
+		phi = np.linspace(0, np.pi, 20)
+		theta = np.linspace(0, 2 * np.pi, 40)
+		x = np.outer(np.sin(theta), np.cos(phi))
+		y = np.outer(np.sin(theta), np.sin(phi))
+		z = np.outer(np.cos(theta), np.ones_like(phi))
 
-	fig, ax = plt.subplots(1, 1, subplot_kw={'projection':'3d'})
-	for key in C.keys():
-		xi, yi, zi = sphere_jit[:, np.array(C[key])]
+		fig, ax = plt.subplots(1, 1, subplot_kw={'projection':'3d'})
+		for key in C.keys():
+			xi, yi, zi = sphere_jit[:, np.array(C[key])]
 
-		ax.scatter(xi, yi, zi, s=100, zorder=10)
+			ax.scatter(xi, yi, zi, s=100, zorder=10)
 
-	plt.show()
+		plt.show()
 	return M, C
 
 M, C = k_fig(10)
-from scipy import spatial
-delaunay = spatial.Delaunay(np.transpose(sphere_jit[:, np.array(M)]))
 
-from mveh import find_mveh
-for j in range(delaunay.simplices.shape[0]):
-	goober = find_mveh(sphere_jit[:, delaunay.simplices[j, :]])
-	print(goober)
+Cs = dict((j, sphere_jit[:,C[j]]) for j in range(len(M)))
+
+Covs = dict((j, np.cov(Cs[j])) for j in range(len(M)))
+eigVals = {}
+eigVecs = {}
+for j in range(len(M)):
+	eigVals[j], eigVecs[j] = np.linalg.eigh(Covs[j])
+
+for j in range(len(M)):
+	print(eigVals[j])
+	print(eigVecs[j])
+	print("\n")

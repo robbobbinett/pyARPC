5c0bef63b9afad6ad97d244bc23423478c897baf
robbobbinett <robinettryan8@gmail.com>
2020-04-06 15:01:51 -0500

    Euclidean mean of quotiented eigenvectors working; need to make it mean over S^{n-1}

--- experiments/general_dr.py
+++ experiments/general_dr.py
@@ -1,4 +1,5 @@
 import numpy as np
+from k_medoids import kMedoids
 
 def reduce_eigendicts(eigVals, eigVecs, d):
 	new_vals = {}
@@ -11,3 +12,37 @@ def reduce_eigendicts(eigVals, eigVecs, d):
 		new_vecs[key] = [vecs[val[0]][1] for val in vals[0:d]]
 
 	return new_vals, new_vecs
+
+def choose_mean_quotient_eigenvector(eigVecs):
+	"""
+	This generates the mean eigenvector from a distribution of
+	normalized eigenvectors. The routine used to generate these
+	normalized eigenvectors---np.linalg.eigh---does not account
+	for that there exist exactly two unit-length eigenvectors
+	corresponding to each eigenspace.
+
+	This routine finds the mean eigenvector after reflecting one
+	of the two clusters of eigenvectors such that the dot product
+	of any two eigenvectors in the sample post-translation is
+	positive.
+
+	Clustering is done by 2-medoids.
+	"""
+	# get number of eigenvectors
+	N = len(eigVecs)
+
+	# populate distance matrix
+	dist_mat = np.zeros((N, N))
+	for j in range(N):
+		for k in range(N):
+			dist_mat[j, k] = np.linalg.norm(eigVecs[j,:]-eigVecs[k,:])
+
+	# get clusters from 2-medoids
+	_, C = kMedoids(dist_mat, 2)
+
+	# reflect all vectors in second cluster
+	newEigVecs = eigVecs.copy()
+	for ind in C[1]:
+		newEigVecs[ind, :] = -eigVecs[ind, :]
+
+	return np.mean(newEigVecs, axis=0)
